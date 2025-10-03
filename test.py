f40667de7b306f6d8268d75291efbe1f0c1ba580
Dimitris Pavlou <82642130+DimitrisPavlou@users.noreply.github.com>
2025-10-03 11:48:04 +0300

    Update test.py
    added the expanded env case

--- test.py
+++ test.py
@@ -1,19 +1,31 @@
 # train_and_eval.py
 from stable_baselines3 import PPO, DQN
-from stable_baselines3.common.env_util import make_vec_env
-from env import *
-from helper_utils import test_trained_model, make_env
+from helper_utils import test_trained_model
 
-# Create environment
-grid_size = 6
-num_obstacles = 5
+# Create vectorized environment
+grid_size = (6,6)
 max_steps = 50
-env = make_vec_env(lambda: make_env(grid_size, num_obstacles, max_steps), n_envs=8)
 
-# correct loading of agents
-model1 = PPO.load("ppo_gridworld", env = env)
-model2 = DQN.load("dqn_gridworld", env = env)
+
+num_obstacles = 5
+# load models
+model1 = PPO.load("agent_parameters/ppo_gridenv")
+model2 = DQN.load("agent_parameters/dqn_gridenv")
 print("Starting evaluation...")
-# run the trained models on the environment
-test_trained_model(model1, grid_size, num_obstacles, max_steps, num_episodes=1)
-test_trained_model(model2, grid_size, num_obstacles, max_steps, num_episodes=1)
+
+# Save the final model
+print("PPO Results")
+test_trained_model(model1, grid_size, num_obstacles, max_steps, expanded=False, num_episodes=100, render = False, print_every=10)
+print("DQN results")
+test_trained_model(model2, grid_size, num_obstacles, max_steps, expanded=False, num_episodes=100, render = False, print_every=10)
+
+# load the expanded env models
+model1_exp = PPO.load("agent_parameters/ppo_expanded_gridenv")
+model2_exp = DQN.load("agent_parameters/dqn_expanded_gridenv")
+
+num_bonus = 2
+num_obstacles_expanded = 3
+print("PPO Results")
+test_trained_model(model1_exp, grid_size, num_obstacles_expanded, max_steps, num_bonus=num_bonus, num_episodes=100, render=False, expanded=True)
+print("DQN Results")
+test_trained_model(model2_exp, grid_size, num_obstacles_expanded, max_steps, num_bonus=num_bonus, num_episodes=100, render=False, expanded=True)
